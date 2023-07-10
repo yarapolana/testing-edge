67adfde96b5332bd85a7d98ef648f936dd19b33f
Yara Polana <yara.polana@gmail.com>
2023-07-10 15:54:23 +0000

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 
+export const config = { runtime: 'edge' };
+
 export const meta: V2_MetaFunction = () => {
   return [
     { title: "New Remix App" },
